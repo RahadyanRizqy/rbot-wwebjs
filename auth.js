7a0a18ef7d33bd059cec88becab24cc8393ab19d
RahadyanRizqy <rahadyanrizqy@gmail.com>
2024-01-28 22:30:25 +0700

    adding auth.js after auth bug

---
+++ auth.js
@@ -0,0 +1,28 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const client = new Client({
+    puppeteer: {
+        headless: true,
+        args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+    },
+    ffmpeg: './ffmpeg.exe',
+    authStrategy: new LocalAuth({ clientId: 'wabot-wwebjs'}),
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+	if (message.body === '.test') {
+		await message.reply('Success');
+	}
+});
+ 
+client.initialize();
+ 
